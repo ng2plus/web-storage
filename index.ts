ac7a57cc837db61b7066bc8e7c0b22e3b55c4a8d
Alexey Lizurchik <al.lizurchik@gmail.com>
2016-09-08 17:38:52 +0300

    * (fix) return removed import

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+import {WebStorageService} from './src/web-storage.service';
+
 export {WebStorageService} from './src/web-storage.service';
 export {WEB_STORAGE_SERVICE_CONFIG, webStorageConfigDefault} from './src/web-storage.config';
 export {WebStorage} from './src/web-storage';
